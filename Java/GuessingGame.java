a6e254453e8ff3969f1ba1dcbd51fa0fd359351e
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-01-27 21:09:06 -0600

    Update GuessingGame.java

--- Java/GuessingGame.java
+++ Java/GuessingGame.java
@@ -22,17 +22,13 @@ public class GuessingGame {
     public static String determineGuess(int userAnswer, int computerNumber, int count) {
         if (userAnswer <=0 || userAnswer >100) {
             return "Your guess is invalid";
-        }
-        else if (userAnswer == computerNumber ){
+        } else if (userAnswer == computerNumber ){
             return "Correct!\nTotal Guesses: " + count;
-        }
-        else if (userAnswer > computerNumber) {
+        } else if (userAnswer > computerNumber) {
             return "Your guess is too high, try again.\nTry Number: " + count;
-        }
-        else if (userAnswer < computerNumber) {
+        } else if (userAnswer < computerNumber) {
             return "Your guess is too low, try again.\nTry Number: " + count;
-        }
-        else {
+        } else {
             return "Your guess is incorrect\nTry Number: " + count;
         }
     }

d55ce0de53e483b5161e9d95d6ea878f69bec173
RileyMoore01 <50551379+RileyMoore01@users.noreply.github.com>
2022-11-29 08:44:28 -0600

    Create TripLaser.c

---
+++ C/TripLaser.c
@@ -0,0 +1,28 @@
+// Read in a lasers light using an LDR sensor and Raspberry Pi
+
+#define LASER_PIN 27
+#include<stdio.h>
+#include<wiringPi.h>
+
+int main(void)
+{
+  wiringPiSetupGpio();
+  pinMode(LASER_PIN,OUTPUT);
+  while(1)
+  {
+    digitalRead(LASER_PIN,HIGH);
+    if(digitalRead(Laser_PIN)==HIGH)
+    {
+      printf("Laser On");
+      printf("\n");
+      delay(4000);
+    }
+    digitalWrite(LASER_PIN,LOW);
+    if(digitalRead(LASER_PIN)==LOW)
+    {
+      printf("Laser Off");
+      printf("\n");
+      dealy(1000);
+    }
+  }
+}
